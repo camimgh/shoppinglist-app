a3ab1cf68f7df13a05f386a44a258b56d18b30bd
camimgh <josephinecmejia@gmail.com>
2020-07-01 10:51:22 -0400

    Added AppState interface.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component<{}, {}> {
+interface AppState {
+  items: Array<string>
+}
+class App extends React.Component<{}, AppState> {
+
   public render() {
     return (
       <div className="App">
