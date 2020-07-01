1d4462f91ffec0000184a08e9fad070a2996fd4d
camimgh <josephinecmejia@gmail.com>
2020-07-01 10:41:28 -0400

    Second commit. Changed into class.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Shopping App</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>My Shopping App</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
